a491ee1335a4533e690aa4c5bf9947f945d3aaab
Eirik Engmo Pettersen <epe083@post.uit.no>
2022-01-30 03:46:16 +0100

    Integrate Isak's fancy chrome extension!

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
-	"time"
 )
 
-type Config struct {
-	PlaybackSpeed     float64 `json:"PlaybackSpeed"`
-	TimestampBuffer   int64   `json:"TimestampBuffer"`
-	PlaybackStartTime int64   `json:"PlaybackStartTime"`
+type ReplayTimeStamper struct {
+	Buffer     int
+	Cmds       map[string]string
+	OutputFile *os.File
+	Exit       chan bool
 }
 
-var cmdPath = flag.String("c", "commands/millia.txt", "Path to command file")
+type KeyEvent struct {
+	Title    string  `json:"title"`
+	Timestep float64 `json:"timestep"`
+	Key      string  `json:"key"`
+}
 
+var KeyCmdPathFlag = flag.String("c", "millia.txt", "Key command filename")
+var bufferFlag = flag.Int("b", 3, "Timestamp Buffer Window")
+var destinationFlag = flag.String("d", "result.txt", "Result destination")
+
+//Converts seconds to hours:minutes:seconds
 func convertSeconds(seconds int) string {
 	if seconds < 0 {
 		seconds = 0
@@ -30,69 +41,80 @@ func convertSeconds(seconds int) string {
 	return fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
 }
 
+//Parses json input, writes timestamp to file if input is registered
+func (rt *ReplayTimeStamper) handleInput(w http.ResponseWriter, req *http.Request) {
+	var event KeyEvent
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Panic("Invalid input")
+	}
+	err = json.Unmarshal(data, &event)
+	if err != nil {
+		log.Panic("json invalid")
+	}
+
+	description, ok := rt.Cmds[event.Key]
+
+	if ok {
+		seconds := int(event.Timestep) - int(rt.Buffer)
+		timeStamp := convertSeconds(seconds)
+		output := fmt.Sprint(timeStamp, " - ", description)
+		fmt.Println(output)
+		rt.OutputFile.WriteString(output + "\n")
+	} else if event.Key == "Escape" {
+		rt.Exit <- true
+	} else {
+		fmt.Println("Unbound key:", event.Key)
+	}
+}
+
 func main() {
 	flag.Parse()
-	fmt.Printf("\n===Using commands from path: %s ===\n", *cmdPath)
-	dat, err := os.ReadFile(*cmdPath)
+	fmt.Printf("===Using key commands from file: %s ===\n", *KeyCmdPathFlag)
+	fmt.Printf("Buffer: %d sec\n", *bufferFlag)
+
+	//Read key commands
+	dat, err := os.ReadFile(filepath.Join("keyCommands", *KeyCmdPathFlag))
 	if err != nil {
 		log.Panic("Invalid command path")
 	}
-	commands := map[string]string{}
+	keyCommands := map[string]string{}
+
+	//Add them to map with Key=keybind, Value=TimestampDescription
 	inputLines := strings.Split(string(dat), "\n")
 	for _, line := range inputLines {
 		command := strings.Split(line, "=")
 		if len(command) != 2 {
 			log.Panic("Invalid input:", command)
 		}
-		commands[command[0]] = command[1]
+		keyCommands[command[0]] = command[1]
 	}
-	configPath := "config.json"
-	cfgDat, err := os.ReadFile(configPath)
+
+	//Create destination file
+	f, err := os.Create(*destinationFlag)
 	if err != nil {
 		log.Panic()
 	}
+	defer f.Close()
 
-	var cfg Config
-	err = json.Unmarshal(cfgDat, &cfg)
-	if err != nil {
-		log.Panic()
-	}
-
-	p := fmt.Print
-
-	f, err := os.Create("result.txt")
-	if err != nil {
-		log.Panic()
-	}
-
-	now := time.Now()
-	p("COMMANDS:\n")
-	for k, v := range commands {
+	//List available commands to client
+	fmt.Println("KEY COMMANDS:")
+	for k, v := range keyCommands {
 		fmt.Println(k, ":", v)
 	}
 	fmt.Println("exit", ":", "exits program")
+	fmt.Println("\n===Starting TimeStamper===")
 
-	p("\n===Starting TimeStamper===\n")
-	fmt.Printf("\nStart Time: %s Playback Speed: %.2f Buffer: %d sec\n", convertSeconds(int(cfg.PlaybackStartTime)), cfg.PlaybackSpeed, cfg.TimestampBuffer)
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		input := scanner.Text()
-		description, ok := commands[input]
-		if ok {
-			//Actual time from start
-			currentTime := -int(time.Until(now).Seconds())
-			//Adjust time passed based on playback speed
-			currentTimePlayback := int(float32(currentTime) * float32(cfg.PlaybackSpeed))
-			//Adjust time to reflect playback start time and timestamp buffer
-			adjustedTime := currentTimePlayback + int(cfg.PlaybackStartTime) - int(cfg.TimestampBuffer)
-			timeStamp := convertSeconds(adjustedTime)
-			output := fmt.Sprintln(timeStamp, " - ", description)
-			p(output)
-			f.WriteString(output)
-		}
-
-		if input == "exit" {
-			return
-		}
+	rts := ReplayTimeStamper{
+		Buffer:     int(*bufferFlag),
+		Cmds:       keyCommands,
+		OutputFile: f,
+		Exit:       make(chan bool),
 	}
+	http.HandleFunc("/", rts.handleInput)
+	go http.ListenAndServe("localhost:9393", nil)
+
+	//Exits when rts receives the Escape key as input
+	<-rts.Exit
+	fmt.Println("Shutting down...")
 }
